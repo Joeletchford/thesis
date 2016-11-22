891b289bb7a24e80f8a33657f7734ca4078bb7bd
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-11-22 10:07:06 -0800

    Refactor largely for readability

--- public/js/index.js
+++ public/js/index.js
@@ -1,131 +1,64 @@
-var socket = io.connect('http://150.253.88.217:8080') // connect to
+$(document).ready(function() {
+  // socket stuff
+  var socket = io.connect('http://150.253.88.217:8080')
 
-socket.on('connect', function(data) { // when connected, do something
-  console.log("connected to the server" + socket.id); // log out out id
-})
+  socket.on('connect', function(data) { // when connected, do something
+    console.log("connected to the server" + socket.id); // log out out id
+  })
 
-$(window).mousemove(_.throttle(function(e) {
-  socket.emit('distortMore', true)
-}));
+  $(window).mousemove(_.throttle(function(e) {
+    socket.emit('distortMore', true)
+  }));
 
-/*
- * ----------------------------------------------------------------------
- * allows drag on mobile
- * ----------------------------------------------------------------------
- */
-
- function map(n, start1, stop1, start2, stop2) {
-     return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
-   };
-
-
-! function(a) {
-  function f(a, b) {
-    if (!(a.originalEvent.touches.length > 1)) {
-      a.preventDefault();
-      var c = a.originalEvent.changedTouches[0],
-        d = document.createEvent("MouseEvents");
-      d.initMouseEvent(b, !0, !0, window, 1, c.screenX, c.screenY, c.clientX, c.clientY, !1, !1, !1, !1, 0, null), a.target.dispatchEvent(d)
-    }
-  }
-  if (a.support.touch = "ontouchend" in document, a.support.touch) {
-    var e, b = a.ui.mouse.prototype,
-      c = b._mouseInit,
-      d = b._mouseDestroy;
-    b._touchStart = function(a) {
-      var b = this;
-      !e && b._mouseCapture(a.originalEvent.changedTouches[0]) && (e = !0, b._touchMoved = !1, f(a, "mouseover"), f(a, "mousemove"), f(a, "mousedown"))
-    }, b._touchMove = function(a) {
-      e && (this._touchMoved = !0, f(a, "mousemove"))
-    }, b._touchEnd = function(a) {
-      e && (f(a, "mouseup"), f(a, "mouseout"), this._touchMoved || f(a, "click"), e = !1)
-    }, b._mouseInit = function() {
-      var b = this;
-      b.element.bind({
-        touchstart: a.proxy(b, "_touchStart"),
-        touchmove: a.proxy(b, "_touchMove"),
-        touchend: a.proxy(b, "_touchEnd")
-      }), c.call(b)
-    }, b._mouseDestroy = function() {
-      var b = this;
-      b.element.unbind({
-        touchstart: a.proxy(b, "_touchStart"),
-        touchmove: a.proxy(b, "_touchMove"),
-        touchend: a.proxy(b, "_touchEnd")
-      }), d.call(b)
-    }
-  }
-}(jQuery);
-
-
-
-
-
-////////////////
-
-
-$(document).ready(
-  function() {
-
-    var element = 0;
-
-
-
-    $(window).click(
-      (function() {
-        $('#showall').click(function(e) {
-          $('.project,.imgdrag').show();
-
-        });
-      })
-    );
-    $(function() {
-      var a = 3;
-      $('.project, .imgdrag, .imgdrag2').draggable({
-        axis: "x,y",
-        containment: "body",
-        start: function(event, ui) {
-          $(this).css("z-index", a++);
-        }
-      });
-      $('drag div').click(function() {
-        $(this).addClass('top').removeClass('bottom');
-        $(this).siblings().removeClass('top').addClass('bottom');
-        $(this).css("z-index", a++);
-
-      });
-      $("a").click(function() {
-        $(".top").removeClass("top").addClass("bottom");
-
-        var box = $(this).attr("href");
-        //alert(box);
-        $(box).addClass("top");
-        $(box).css("z-index", a++);
-
-      });
-
-    });
-
+  // sound
+  socket.on('amountToDistort', function(distortionAmount) {
+    // if we see a projectionRectangle message then do some stuff
+    console.log(distortionAmount) //see the coordinates come down.
+    if (distortionAmount > 0) {
+      var sound1 = document.getElementById("audio1");
+      var sound2 = document.getElementById("audio2");
+      var s2vol = volMap(distortionAmount, 0, 1500, 0.0, 1.0)
+      var s1vol = volMap(distortionAmount, 0, 1500, 1.0, 0.0)
+      sound2.volume = s2vol;
+      sound1.volume = s1vol;
+    };
   });
 
+  function volMap(n, start1, stop1, start2, stop2) {
+    return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+  };
 
-// sound
+  // other event listeners
+  var element = 0;
+  var a = 3;
 
-socket.on('amountToDistort', function(distortionAmount) { // if we see a projectionRectangle message then do some stuff
-  console.log(distortionAmount) //see the coordinates come down.
+  $(window).click((function() {
+    $('#showall').click(function(e) {
+      $('.project,.imgdrag').show();
 
-  if (distortionAmount > 0) {
+    });
+  }));
 
-    var sound1 = document.getElementById("audio1");
-    var sound2 = document.getElementById("audio2");
+  $('.project, .imgdrag, .imgdrag2').draggable({
+    axis: "x,y",
+    containment: "body",
+    start: function(event, ui) {
+      $(this).css("z-index", a++);
+    }
+  });
 
+  $('drag div').click(function() {
+    $(this).addClass('top').removeClass('bottom');
+    $(this).siblings().removeClass('top').addClass('bottom');
+    $(this).css("z-index", a++);
+  });
 
-    var s2vol = map(distortionAmount,0,1500,0.0,1.0)
-    var s1vol = map(distortionAmount,0,1500,1.0,0.0)
+  $("a").click(function() {
+    $(".top").removeClass("top").addClass("bottom");
+    var box = $(this).attr("href");
+    //alert(box);
+    $(box).addClass("top");
+    $(box).css("z-index", a++);
+  });
 
-    sound2.volume = s2vol;
-    sound1.volume = s1vol;
-
-
-};
-})
+});
--- public/js/projection.js
+++ public/js/projection.js
@@ -1,52 +1,41 @@
 // video src https://vimeo.com/154586454
 
 var socket = io.connect('http://150.253.88.217:8080')
-  //8080, "10.5.32.145"
-  // var socket = io.connect()
+//set up a place for us to connect to, and try to connect.
 
-function map(n, start1, stop1, start2, stop2) {
-    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
-  };
-
-
-// var socket = io.connect('http://localhost:3000') //set up a place for us to connect to, and try to connect.
 var vid = document.getElementById("bgvid");
 
 socket.on('connect', function(data) { // when we are connected do something.
-  console.log("connected to the server" + socket.id); // log out our socket's id, some long garbled number letter tcching that is unique!
-
+  console.log("connected to the server" + socket.id);
+  // log out our socket's id, some long garbled number letter tcching that is unique!
 })
 
-socket.on('amountToDistort', function(distortionAmount) { // if we see a projectionRectangle message then do some stuff
+socket.on('amountToDistort', function(distortionAmount) {
+  // if we see a projectionRectangle message then do some stuff
   console.log(distortionAmount) //see the coordinates come down.
 
   if (distortionAmount > 1) {
     // what im working with now
-    $(".overlay")
-      .css({
-        'filter': 'saturate(' + distortionAmount * 0.01 + ') blur(' + distortionAmount * 0.03 + 'px)'
-      });
+    $(".overlay").css({
+      'filter': 'saturate(' + distortionAmount * 0.01 + ') blur(' + distortionAmount * 0.03 + 'px)'
+    });
   }
-},200);
+}, 200);
 
-
-
-socket.on('amountToDistort', function(distortionAmount) { // if we see a projectionRectangle message then do some stuff
+socket.on('amountToDistort', function(distortionAmount) {
+  // if we see a projectionRectangle message then do some stuff
   console.log(distortionAmount) //see the coordinates come down.
 
   if (distortionAmount > 0) {
-
     var sound1 = document.getElementById("audio1");
     var sound2 = document.getElementById("audio2");
-
-
-    var s1vol = map(distortionAmount,0,1500,0.0,1.0)
-    var s2vol = map(distortionAmount,0,1500,1.0,0.0)
-
+    var s1vol = volMap(distortionAmount, 0, 1500, 0.0, 1.0)
+    var s2vol = volMap(distortionAmount, 0, 1500, 1.0, 0.0)
     sound1.volume = s1vol;
     sound2.volume = s2vol;
-
-
-
-};
+  };
 })
+
+function volMap(n, start1, stop1, start2, stop2) {
+  return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+};
