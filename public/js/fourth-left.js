17f5e0e94404b1dadca91f07dd63186a0a4a8a02
Joe Letchford <rletchford@mcad.edu>
2017-04-06 13:38:48 -0500

    almost done

--- public/js/fourth-left.js
+++ public/js/fourth-left.js
@@ -1,15 +1,16 @@
+
 //
 // $( ".image" ).draggable({ containment: ".box" });
 
 
 $(document).ready(function() {
     var a = 3;
-    $('#image1, #image2, #image3, #image4').draggable({
+    $('#image, #image1, #image2, #image3, #image4').draggable({
 		scroll: false,
 		 containment: '.outershell',
    	start: function(event, ui) { $(this).css("z-index", a++); }
 });
-    $('#box img').click(function() {
+    $('#box img div').click(function() {
         $(this).addClass('top').removeClass('bottom');
         $(this).siblings().removeClass('top').addClass('bottom');
         $(this).css("z-index", a++);
